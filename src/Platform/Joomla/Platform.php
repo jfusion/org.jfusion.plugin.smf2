177fc237bb5af6326d2ab00846e9ff11a655aa40
Morten Hundevad <morten.hundevad@jfusion.rg>
2014-09-03 16:15:16 +0200

    removed some functions that did not belong in framework

--- src/Platform/Joomla/Platform.php
+++ src/Platform/Joomla/Platform.php
@@ -12,6 +12,7 @@
 use JFusion\Application\Application;
 use JFusion\Factory;
 use JFusion\Framework;
+use JFusion\Parser\Parser;
 use JFusion\User\Userinfo;
 use JFusion\Plugin\Platform\Joomla;
 
@@ -1100,7 +1101,8 @@ HTML;
 			}
 			$options = array();
 			$options['bbcode_patterns'] = $bbcode;
-			$text = Framework::parseCode($text, 'bbcode', $options);
+			$parser = new Parser();
+			$text = $parser->parseCode($text, 'bbcode', $options);
 		} elseif ($for == 'joomla' || ($for == 'activity' && $params->get('parse_text') == 'html')) {
 			$options = array();
 			//convert smilies so they show up in Joomla as images
@@ -1171,9 +1173,11 @@ HTML;
 				$options['html_patterns'][$bb] = array('mode' => 1, 'content' => 0, 'method' => array($this, 'parseCustomBBCode'), 'class' => $class, 'allow_in' => $allow_in);
 			}
 
-			$text = Framework::parseCode($text, 'html', $options);
+			$parser = new Parser();
+			$text = $parser->parseCode($text, 'html', $options);
 		} elseif ($for == 'search') {
-			$text = Framework::parseCode($text, 'plaintext');
+			$parser = new Parser();
+			$text = $parser->parseCode($text, 'plaintext');
 		} elseif ($for == 'activity') {
 			if ($params->get('parse_text') == 'plaintext') {
 				$options = array();
@@ -1182,7 +1186,8 @@ HTML;
 					$status['limit_applied'] = 1;
 					$options['character_limit'] = $params->get('character_limit');
 				}
-				$text = Framework::parseCode($text, 'plaintext', $options);
+				$parser = new Parser();
+				$text = $parser->parseCode($text, 'plaintext', $options);
 			}
 		}
 		return $status;
